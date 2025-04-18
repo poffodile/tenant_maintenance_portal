3b6f245b5daea18fb98d5143b8009b65ebe3ccbc
Precious <lamberville73@gmail.com>
2025-04-19 00:50:02 +0100

    implement ed maintenance request form with validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MaintenanceFormComponent } from './maintenance-form/maintenance-form.component';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MaintenanceFormComponent],
+  imports: [RouterOutlet, FormsModule, MaintenanceFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/maintenance-form/maintenance-form.component.ts
+++ src/app/maintenance-form/maintenance-form.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-maintenance-form',
   standalone: true,
 
-  imports: [],
+  //imports: [],
   templateUrl: './maintenance-form.component.html',
   styleUrl: './maintenance-form.component.css',
+  imports: [FormsModule],
 })
-export class MaintenanceFormComponent {}
+export class MaintenanceFormComponent {
+  formData = {
+    tenantName: '',
+    description: '',
+    urgency: '',
+  };
+
+  submitRequest() {
+    console.log('Maintenance Request Submitted:', this.formData);
+    // TODO: add to a list of requests
+  }
+}
